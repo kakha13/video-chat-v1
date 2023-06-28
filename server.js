c96ff4e3d3b42270c54e9efaa2640f6f494b2003
KAKHA13 <kakhagiorgashvili@gmail.com>
2023-06-28 04:10:01 +0400

    working local

--- server.js
+++ server.js
@@ -24,16 +24,47 @@ app.get("/:room", (req, res) => {
   res.render("room", { roomId: req.params.room });
 });
 
+
+
+io.on("send", (socket) => {
+  console.log(socket)
+})
+
+var allClients = [];
 io.on("connection", (socket) => {
+  allClients.push(socket);
+  console.log("connected")
   socket.on("join-room", (roomId, userId, userName) => {
-    socket.join(roomId);
+    console.log(roomId, userId, userName)
+    socket.join(roomId)
+   
     setTimeout(()=>{
-      socket.to(roomId).broadcast.emit("user-connected", userId);
+      socket.broadcast.emit("user-connected", userId);
+      console.log("first user connected")
     }, 1000)
+
     socket.on("message", (message) => {
-      io.to(roomId).emit("createMessage", message, userName);
+      console.log(message)
+      io.to(roomId).emit("createMessage", message, userName, new Date().toLocaleTimeString());
     });
+
+    // disconected
+    // socket.on('disconnect', function(){
+    //   console.log("disconnected")
+    //   console.log({roomId, userId, userName})
+    //   socket.broadcast.to(roomId).emit('user_leave', userId);
+    // });
+    socket.on('disconnect', function() {
+      console.log('Got disconnect!');
+
+      var i = allClients.indexOf(socket);
+      allClients.splice(i, 1);
+      socket.broadcast.to(roomId).emit('user_leave', userId);
+   });
+
   });
 });
+
+
 
 server.listen(process.env.PORT || 3030);
